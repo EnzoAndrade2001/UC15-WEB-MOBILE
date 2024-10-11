2a62f4ca84d377845b639c7476924479e82d18a4
Enzo de Andrade Cruz <182220019@senacinformatica.br>
2024-10-10 21:52:15 -0300

    alteraçoes aula 10

--- listaCompras/src/assets/js/script.js
+++ listaCompras/src/assets/js/script.js
@@ -3,68 +3,95 @@ const wrapper = document.querySelector(".wrapper");
 const form = wrapper.querySelector("form");
 const liInput = wrapper.querySelector("#liInput");
 const ulLista = document.querySelector(".lista");
+const errorMessage = document.getElementById("error-message");
 let preValue;
+
+function clearError() {
+        errorMessage.textContent= '';
+};
+
+function showError(message) {
+        errorMessage.textContent = message;
+}
 
 form.addEventListener("submit", (event) => {
         event.preventDefault();
 
+        clearError();
+
         let liValue = liInput.value.trim();
 
-
-
-        if (!validarInput() || verificaLista() ) {
+        if (!validarInput()) {
                 return;
         }
 
-        preValue = liValue;
+        if (!verificaLista()) {
+                return;
+        }
 
         let li = document.createElement("li");
         li.innerText = liValue;
-
-
         let deleteBtn = document.createElement("button");
         deleteBtn.innerText = "Deletar";
         deleteBtn.addEventListener("click", () => {
-                ulLista.removeChild(li);
+            ulLista.removeChild(li);
+
+            if(ulLista.children.length === 0) {
+                ulLista.classList.remove("show");
+            } 
         });
 
 
         li.appendChild(deleteBtn);
         ulLista.appendChild(li);
 
+        if(!ulLista.classList.contains("show")){
+                ulLista.classList.add("show");
+        }
 
         liInput.value = '';
 });
 
+
+
+
+
+function showError(message){
+        errorMessage.textContent = message;
+};
+
 function validarInput() {
 
+        clearError();
+
         if (liInput.value.length < 4) {
-                alert("O item da lista de compras deve ter pelo menos 4 caracteres.");
+                showError("O item da lista de compras deve ter pelo menos 4 caracteres.");
                 return false;
         }
 
         if (liInput.value.trim() === "") {
-                alert("O item deve ser valido")
-                return false;
-        }
-
-        if (!liInput.value.trim() || preValue === liInput.value.trim()) {
-                alert("Preencha o campo para adicionar o item");
+                showError("O item deve ser valido")
                 return false;
         }
 
         return true;
-}
+};
 
 function verificaLista() {
-        for(ulLista = 0; ulLista < li.length; ulLista++){
-                console.log(ulLista[li]);
+        const liItems = ulLista.querySelectorAll("li");
+        for(let li of liItems){
+                if(li.innerText.replace("Deletar", "").trim() === liInput.value.trim()) {
+                        showError("O item já está na lista.");
+                        return false;
+                }
         }
-}
+        return true;
+};
 
 
 
-
+/*
+*/
 
 
 
